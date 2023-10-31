9e489c734f706d0043ed2fb3ba7e43b34ab55913
k1s3n <christoffer@rehnmark.se>
2023-10-31 17:16:53 +0100

    get tasks

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    #categories = db.Column(db.String(200), nullable=False)
+    categories = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+    def as_dict(self):
+        
+        task_dict = {}
+        
+        for column in self.__table__.columns:
+            
+            column_name = column.name
+            column_value = getattr(self, column_name)
+            
+            task_dict[column_name] = column_value
+        
+        return task_dict
     
 
 
-#dkhejdawda
 @app.route("/")
 def home():
      return render_template("base.html")
@@ -27,9 +38,13 @@ def home():
 
 # #GET /tasks Hämtar alla tasks. För VG: lägg till en parameter completed som kan filtrera på färdiga eller ofärdiga tasks.
 
-# @app.route("/tasks", methods=['GET'])
-# def get_tasks():
-#     return "get all tasks"
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+    tasks = Todo.query.all()
+    if not tasks:
+        return jsonify({"msg": "No task found."}), 404
+    task_list = [task.as_dict() for task in tasks]
+    return jsonify(task_list)
 
 # #POST /tasks Lägger till en ny task. Tasken är ofärdig när den först läggs till.
 
