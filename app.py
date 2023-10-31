39b2fe193a7ec4aa6d65110bc6571e4280e677a0
k1s3n <christoffer@rehnmark.se>
2023-10-31 14:44:43 +0100

    get tasks

--- app.py
+++ app.py
@@ -6,5 +6,27 @@ app = Flask(__name__)
 def home():
     return render_template("base.html")
 
+#backend
+
+#GET /tasks Hämtar alla tasks. För VG: lägg till en parameter completed som kan filtrera på färdiga eller ofärdiga tasks.
+
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+    return "get all tasks"
+
+#POST /tasks Lägger till en ny task. Tasken är ofärdig när den först läggs till.
+
+# GET /tasks/{task_id} Hämtar en task med ett specifikt id.
+
+# DELETE /tasks/{task_id} Tar bort en task med ett specifikt id.
+
+# PUT /tasks/{task_id} Uppdaterar en task med ett specifikt id.
+
+# PUT /tasks/{task_id}/complete Markerar en task som färdig.
+
+# GET /tasks/categories/ Hämtar alla olika kategorier.
+
+# GET /tasks/categories/{category_name} Hämtar alla tasks från en specifik kategori.
+
 if __name__ == "__main__":
     app.run(debug=True)
